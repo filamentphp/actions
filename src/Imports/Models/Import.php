2f400b0c6b3fd2d2708f9d97888a235461319101
Dan <git@danharrin.com>
2025-06-12 15:01:45 +0000

    Merge pull request #16491 from filamentphp/fix/windows-import-file-validation

--- src/Imports/Models/Import.php
+++ src/Imports/Models/Import.php
@@ -2,9 +2,7 @@
 
 namespace Filament\Actions\Imports\Models;
 
-use App\Models\User;
 use Carbon\CarbonInterface;
-use Exception;
 use Filament\Actions\Imports\Importer;
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Collection;
@@ -52,17 +50,8 @@ class Import extends Model
 
         $authenticatable = app(Authenticatable::class);
 
-        if ($authenticatable) {
-            /** @phpstan-ignore-next-line */
-            return $this->belongsTo($authenticatable::class);
-        }
-
-        if (! class_exists(User::class)) {
-            throw new Exception('No [App\\Models\\User] model found. Please bind an authenticatable model to the [Illuminate\\Contracts\\Auth\\Authenticatable] interface in a service provider\'s [register()] method.');
-        }
-
         /** @phpstan-ignore-next-line */
-        return $this->belongsTo(User::class);
+        return $this->belongsTo($authenticatable::class);
     }
 
     /**
